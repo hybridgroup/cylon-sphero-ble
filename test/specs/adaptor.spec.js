77dbd021cae57b456d0129a1f6b794896ce3b22d
deadprogram <ron@hybridgroup.com>
2014-10-10 14:35:28 -0700

    Initial commit

---
+++ test/specs/adaptor.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var Ollie = source("adaptor");
+
+describe("Cylon.Adaptors.Ollie", function() {
+  var adaptor = new Ollie();
+
+  it("needs tests");
+});
---
+++ test/specs/driver.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Ollie = source("driver");
+
+describe("Cylon.Drivers.Ollie", function() {
+  var driver = new Ollie({
+    device: { connection: 'connect' }
+  });
+
+  it("needs tests");
+});
