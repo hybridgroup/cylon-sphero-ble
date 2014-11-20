8961584fd91dedc3e88d59d1d6ec10865fcd6da9
deadprogram <ron@hybridgroup.com>
2014-11-20 08:01:31 -0800

    Some WIP on Ollie control

--- test/specs/cylon-ollie.spec.js
+++ test/specs/cylon-ollie.spec.js
@@ -5,12 +5,6 @@ var module = source("cylon-ollie");
 var Driver = source('driver');
 
 describe("Cylon.Ollie", function() {
-  describe("#register", function() {
-    it("should be a function", function() {
-      expect(module.register).to.be.a('function');
-    });
-  });
-
   describe("#driver", function() {
     it("returns an instance of the Driver", function() {
       var args = { device: {connection: 'test'} };
